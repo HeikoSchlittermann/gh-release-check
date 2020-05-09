7f8181cadee8724f53d85df2648c45bfbb7ff09c
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2020-05-09 23:06:42 +0200

    Works for rocketchat desktop

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
-module main.go
+module schlittermann.de/go/github-release-check
 
-require (
-	github.com/antchfx/htmlquery v1.2.2
-	github.com/antchfx/xpath v1.1.4 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
-)
+go 1.14
--- main.go
+++ main.go
@@ -1,22 +1,94 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"log"
-
-	"github.com/antchfx/htmlquery"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
 )
 
-func main() {
-	doc, err := htmlquery.LoadURL("https://github.com/RocketChat/Rocket.Chat.Electron/releases")
+const (
+	URL = "https://api.github.com/repos/RocketChat/Rocket.Chat.Electron/releases/latest"
+)
 
+var (
+	o = struct {
+		debug  *bool
+		format *string
+	}{
+		debug:  flag.Bool("debug", false, "switch on debugging"),
+		format: flag.String("format", "{{.Name}} {{.Tarball_url}}\n", "the `format` of the output"),
+	}
+)
+
+type latest struct {
+	Tarball_url  string
+	Published_at time.Time
+	Name         string
+	Tag_name     string
+	Assets       []struct {
+		Name                 string
+		Browser_download_url string
+		Content_type         string
+		Updated_at           time.Time
+	}
+	Deb_url string
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options]\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), `
+    Format string elements:
+      .Name             Name of the latest release
+      .Tag_name         Name of the release tag
+      .Tarball_url      URL of the Tarball for download
+      .Published_date   Date of publication
+      .Assets []
+        .Name
+        .Browser_downlaod_url
+        .Content_type
+        .Updated_at
+`)
+	}
+	flag.Parse()
+
+	var tt = template.New("output")
+	if _, err := tt.Parse(*o.format); err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := http.Get(URL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	list := htmlquery.Find(doc, "//a")
+	if *o.debug {
+		io.Copy(os.Stdout, response.Body)
+		return
+	}
 
-	fmt.Printf("%v\n", list)
+	var latest latest
+	if err := json.NewDecoder(response.Body).Decode(&latest); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, a := range latest.Assets {
+		if strings.HasSuffix(a.Name, ".deb") {
+			latest.Deb_url = a.Browser_download_url
+			break
+		}
+	}
+
+	tt.Execute(os.Stdout, latest)
 
 }
